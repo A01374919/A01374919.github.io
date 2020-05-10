19b11caa4fc94379681a434a2d92aaf6455cd80a
Pedro Cortés Soberanes <30931239+A01374919@users.noreply.github.com>
2020-05-09 21:29:18 -0500

    new

--- main.js
+++ main.js
@@ -1,5 +1,81 @@
-import productsdb,{ bulkcreate,getData,createEle,Sortobj } from './module.js';
+//import productsdb,{ bulkcreate,getData,createEle,Sortobj } from './module.js';
 
+
+const productsdb = (dbname, table) => {
+   const db = new Dexie(dbname);
+   db.version(1).stores(table);
+   db.open();
+
+   return db;
+ };
+
+ const bulkcreate = (dbtable, data) => {
+   let flag = empty(data);
+   if (flag) {
+     dbtable.bulkAdd([data]);
+     console.log("se ha ingresado de manera correcta");
+   } else {
+     console.log("completa los campos de informacion");
+   }
+   return flag;
+
+ };
+
+const empty = object => {
+   let flag = false;
+
+   for (const value in object) {
+     if (object[value] != "" && object.hasOwnProperty(value)) {
+       flag = true;
+     } else {
+       flag = false;
+     }
+   }
+   return flag;
+ };
+
+const getData = (dbtable,fn) =>{
+
+   let index = 0;
+   let obj={};
+
+   dbtable.count((count)=>{
+      if(count){
+         dbtable.each(table =>{
+           obj =Sortobj(table);
+
+           fn(obj,index++);
+
+         })
+      }
+      else{
+          fn(0);
+      }
+   })
+}
+
+const Sortobj = sortobj =>{
+   let obj = {};
+   obj={
+      id: sortobj.id,
+      bookName: sortobj.bookName,
+      editorial: sortobj.editorial,
+      encargado: sortobj.encargado,
+      matricula: sortobj.matricula
+
+   }
+
+   return obj;
+}
+
+const createEle=(tagname,appendTo,fn)=>{
+    const element =document.createElement(tagname);
+    if(appendTo)appendTo.appendChild(element);
+    if(fn)fn(element);
+
+}
+
+///////////////////////////////
 
 let db =productsdb("Productdb",{
    products:`++id,bookName,editorial,encargado,matricula`
@@ -46,7 +122,7 @@ function table(){
                createEle("td",tr, td=>{
                   td.textContent = data.matricula === data[value]? `${data[value]}`: data[value];
                })
-               
+
             }
             createEle("td",tr,td=>{
                createEle("i",td,i=>{
@@ -64,7 +140,7 @@ function table(){
                   i.onclick= deletebtn;
                })
             })
-            
+
          })
       }
       else{
